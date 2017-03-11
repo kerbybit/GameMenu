aadb3eeeb6084749b83b2b4057bdbb8a002492db
kerbybit <kerbybit@gmail.com>
2017-03-10 20:22:14 -0600

    Improved animations
    added closing and opening animation

--- src/main/java/com/kerbybit/GameMenu/MenuGUI.java
+++ src/main/java/com/kerbybit/GameMenu/MenuGUI.java
@@ -84,7 +84,7 @@ public class MenuGUI extends GuiScreen {
         if (button==0) {
             if (menu == 0) {
                 if (y > height - (height/7)) {
-                    MC.thePlayer.closeScreen();
+                    menu = 1;
                 } else {
                     if (x < (width / 2) - 5) {
                         if (x > buttonTextOffset_pvp-100 && x < buttonTextOffset_pvp+100) {
@@ -117,12 +117,12 @@ public class MenuGUI extends GuiScreen {
     }
 
     private void mouseHover(int x, int y) {
-        if (menu == 0) {
+        if (menu == 0 || menu == -1) {
             if (y > height - (height/7)) {
-                buttonOffset_pvp = floor(Animation.interp(buttonOffset_pvp, -40, 5, 1));
+                buttonOffset_pvp = floor(Animation.interp(buttonOffset_pvp, -100, 5, 1));
                 buttonTextOffset_pvp = floor(Animation.interp(buttonTextOffset_pvp, width/4, 5, 1));
                 buttonUnderlineOffset_pvp = floor(Animation.interp(buttonUnderlineOffset_pvp, 0, 5, 1));
-                buttonOffset_arcade = floor(Animation.interp(buttonOffset_arcade, -40, 5, 1));
+                buttonOffset_arcade = floor(Animation.interp(buttonOffset_arcade, -100, 5, 1));
                 buttonTextOffset_arcade = floor(Animation.interp(buttonTextOffset_arcade, width - width/4, 5, 1));
                 buttonUnderlineOffset_arcade = floor(Animation.interp(buttonUnderlineOffset_arcade, 0, 5, 1));
                 for (int i=0; i<buttons_arcade.size(); i++) {
@@ -134,7 +134,7 @@ public class MenuGUI extends GuiScreen {
                 buttonOffset_exit = floor(Animation.interp(buttonOffset_exit,  -height/7, 5, 1));
             } else {
                 buttonOffset_exit = floor(Animation.interp(buttonOffset_exit, 0, 5, 1));
-                if (x < (width/2)-5) {
+                if (x < width/2) {
                     for (int i=0; i<buttons_arcade.size(); i++) {
                         buttonsOffset_arcade.set(i, floor(Animation.interp(buttonsOffset_arcade.get(i), 0, 5, 1)));
                     }
@@ -158,7 +158,7 @@ public class MenuGUI extends GuiScreen {
                             buttonsOffset_pvp.set(i, floor(Animation.interp(buttonsOffset_pvp.get(i), 0, 5, 1)));
                         }
                     }
-                } else if (x > (width/2)+5) {
+                } else {
                     for (int i=0; i<buttons_pvp.size(); i++) {
                         buttonsOffset_pvp.set(i, floor(Animation.interp(buttonsOffset_pvp.get(i), 0, 5, 1)));
                     }
@@ -184,13 +184,35 @@ public class MenuGUI extends GuiScreen {
                     }
                 }
             }
+        } else {
+            buttonOffset_pvp = floor(Animation.interp(buttonOffset_pvp, -width/2-20, 10, 1));
+            buttonTextOffset_pvp = floor(Animation.interp(buttonTextOffset_pvp, -width/2-20 + width/4, 10, 1));
+            buttonUnderlineOffset_pvp = floor(Animation.interp(buttonUnderlineOffset_pvp, 0, 5, 1));
+
+            buttonOffset_arcade = floor(Animation.interp(buttonOffset_arcade, -width/2-20, 10, 1));
+            buttonTextOffset_arcade = floor(Animation.interp(buttonTextOffset_arcade, width/2+20 + width - width/4, 10, 1));
+            buttonUnderlineOffset_arcade = floor(Animation.interp(buttonUnderlineOffset_arcade, 0, 6, 1));
+
+            for (int i=0; i<buttons_arcade.size(); i++) {
+                buttonsOffset_arcade.set(i, floor(Animation.interp(buttonsOffset_arcade.get(i), 0, 5, 1)));
+            }
+            for (int i=0; i<buttons_pvp.size(); i++) {
+                buttonsOffset_pvp.set(i, floor(Animation.interp(buttonsOffset_pvp.get(i), 0, 5, 1)));
+            }
+
+            buttonOffset_exit = floor(Animation.interp(buttonOffset_exit,  21, 10, 1));
+
+            System.out.println(buttonOffset_exit);
+            if (buttonOffset_exit == 12) {
+                MC.thePlayer.closeScreen();
+            }
         }
     }
 
     @Override
     public void drawScreen(int x, int y, float ticks) {
         GL11.glColor4f(1, 1, 1, 1);
-        drawDefaultBackground();
+        //drawDefaultBackground();
 
         ScaledResolution var5 = new ScaledResolution(MC);
         float width = var5.getScaledWidth();
@@ -203,10 +225,10 @@ public class MenuGUI extends GuiScreen {
 
 
         if (menu == -1) {
-            buttonTextOffset_pvp = floor(width/4);
-            buttonTextOffset_arcade = floor(width - width/4);
-            buttonOffset_pvp = 0;
-            buttonOffset_arcade = 0;
+            buttonTextOffset_pvp = floor(-width/2-20);
+            buttonTextOffset_arcade = floor(width/2+20 + width - width/4);
+            buttonOffset_pvp = floor(-width/2-20);
+            buttonOffset_arcade = floor(-width/2-20);
             buttonUnderlineOffset_pvp = 0;
             buttonUnderlineOffset_arcade = 0;
             for (int i=0; i<buttonsOffset_pvp.size(); i++) {
@@ -219,7 +241,7 @@ public class MenuGUI extends GuiScreen {
             menu = 0;
         }
 
-        if (menu == 0) {
+        if (menu == 0 || menu == 1) {
             drawRect(0, 0, floor(width/2) - 5 + buttonOffset_pvp, floor(height), 0x50000000);
             drawCenteredString(ren, buttonText_pvp, buttonTextOffset_pvp, floor(height/4), 0xffffffff);
             if (buttonUnderlineOffset_pvp > 0) {
@@ -233,7 +255,7 @@ public class MenuGUI extends GuiScreen {
                 int drawO = buttonsOffset_pvp.get(i);
                 int drawY = floor(height/4)+20 + (i*17);
 
-                drawRect(drawX-(drawW/2)-3-drawO, drawY-3, drawX+(drawW/2)+3+drawO, drawY+11, 0xff000000);
+                drawRect(drawX-(drawW/2)-3-drawO, drawY-3, drawX+(drawW/2)+3+drawO, drawY+11, 0xff00102b);
                 drawCenteredString(ren, button, drawX, drawY, 0xffffffff);
                 i++;
             }
@@ -251,13 +273,13 @@ public class MenuGUI extends GuiScreen {
                 int drawO = buttonsOffset_arcade.get(i);
                 int drawY = floor(height/4)+20 + (i*17);
 
-                drawRect(drawX-(drawW/2)-3-drawO, drawY-3, drawX+(drawW/2)+3+drawO, drawY+11, 0xff000000);
+                drawRect(drawX-(drawW/2)-3-drawO, drawY-3, drawX+(drawW/2)+3+drawO, drawY+11, 0xff001c03);
                 drawCenteredString(ren, button, drawX, drawY, 0xffffffff);
                 i++;
             }
 
             drawRect(0, floor(height + buttonOffset_exit), floor(width), floor(height), 0xff000000);
-            drawCenteredString(ren, buttonText_exit, floor(width / 2), floor(height + buttonOffset_exit + 12), 0xffffffff);
+            drawCenteredString(ren, buttonText_exit, floor(width / 2), floor(height + buttonOffset_exit/2), 0xffffffff);
         }
 
         super.drawScreen(x, y, ticks);
